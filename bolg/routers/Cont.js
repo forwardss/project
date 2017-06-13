b86dcc4dc367d338eec94595b68641f6e0992926
forwards <2422537496@qq.com>
2017-06-13 08:02:30 +0800

    a

--- bolg/routers/Cont.js
+++ bolg/routers/Cont.js
@@ -5,7 +5,7 @@ router.post("/Cont", function(req,res){
 	let url = req.body.url;
 	connection.query("select content,num from bolg where No='"+url+"'", function(err,data){
 		if(data.length>0){
-			var num = data[0].num+1;
+			let num = data[0].num+1;
 			connection.query("update bolg set num='"+num+"' where No='"+url+"'", function(err,data){
 			})
 			res.send({msg:1,data:data});
